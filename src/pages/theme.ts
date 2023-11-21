0c501804db747364d173e165a41da36558861673
Adedeji Adelanwa <adedejiadelanwa@gmail.com>
2023-11-21 22:05:18 +0100

    chore: Initial setup

---
+++ src/pages/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+    brand: {
+      "peabuxblue": '#010350',
+      800: '#153e75',
+      700: '#2a69ac',
+    },
+  }
+
+  export const theme = extendTheme({ colors })
